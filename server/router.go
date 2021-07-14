e76cc3def1aa33b8e2ea37cdaee49970c89820ea
Raffy E <raffy27@pm.me>
2021-07-14 13:51:59 +0300

    Serving static files

--- server/router.go
+++ server/router.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/Raffy27/go-purple/controllers"
 	"github.com/Raffy27/go-purple/middleware"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,9 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	// Static files
+	r.Use(static.Serve("/", static.LocalFile("public", false)))
 
 	test := new(controllers.TestController)
 	r.GET("/ping", test.Ping)
